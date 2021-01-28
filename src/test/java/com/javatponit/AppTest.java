ce3829c473f42193378e8d1f258641dfd2bf8755
BPatil@leaseaccelerator.com <BPatil@leaseaccelerator.com>
2021-01-28 20:08:51 +0530

    first commit

---
+++ src/test/java/com/javatponit/AppTest.java
@@ -0,0 +1,21 @@
+package com.javatponit;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest extends TestCase {
+	
+	public AppTest(String testName)
+	{
+		super(testName);
+	}
+ public static Test Suite() {
+	 
+	 return  new TestSuite(AppTest.class);
+ }
+ public void testApp()
+ {
+	 assertTrue(true);
+ }
+}
